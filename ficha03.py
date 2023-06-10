64460552552405481e67e6b71892b975bcbe8d80
ricardo-henriques-silva <134967737+ricardo-henriques-silva@users.noreply.github.com>
2023-06-10 19:23:27 +0100

    Opções e módulo 1

---
+++ ficha03.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 10 18:45:16 2023
+
+@author: rhcsilva
+"""
+
+    
+
+personagem = {
+    "nome": input("Introduza o nome da personagem:\n",
+    "idade": input("Introduza a idade da personagem:\n")
+
+    }
