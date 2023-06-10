f2f27006d893c7db260e95fca16997568b76bd12
ricardo-henriques-silva <134967737+ricardo-henriques-silva@users.noreply.github.com>
2023-06-10 19:32:04 +0100

    Opções e Módulo 1

--- ficha03.py
+++ ficha03.py
@@ -1,14 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 10 18:45:16 2023
+# Inicialização da lista de personagens
+personagens =[]
 
-@author: rhcsilva
-"""
+print("\n===Programa de criação e manipulação de perfis de personagens===\n")
+print("===Escolha uma das seguintes Funcionalidades===\n")
+print("1. Adicionar um novo perfil de personagem")
+print("2. Modificar um perfil de personagem existente")
+print("3. Remover um perfil de personagem")
+print("4. Visualizar a lista completa de personagens")
+print("5. Pesquisar um personagem específico pelo nome")
+modulo=input("Escolha uma opção de 1 a 5:\n")
 
-    
-
-personagem = {
-    "nome": input("Introduza o nome da personagem:\n",
-    "idade": input("Introduza a idade da personagem:\n")
-
+# Funcionalidade 1 Novo perfil
+novapersonagem = {
+    "nome": input("Introduza o nome da personagem:\n"),
+    "idade": input("Introduza a idade da personagem:\n"),
+    "profissão": input("Introduza a profissão da personagem:\n"),
+    "hobbies": input("Introduza os hobbies da personagem, separados por ;\n").split(";"),
+    "descrição": input("Introduza uma descrição da personagem:\n")
     }
+personagens.append(novapersonagem)
+print(personagens)
